88bed994583544d10be40ddd7c8636c3df33850e
Abuka-Victor <abukavictoro@gmail.com>
2025-06-14 01:23:50 +0100

    Add final code: frontend

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -72,13 +72,23 @@ const Sidebar = () => {
                   rounded-full ring-2 ring-zinc-900"
                 />
               )}
+              {user?.id === 'chatbot' && (
+                <span
+                  className="absolute bottom-0 right-0 size-3 bg-green-500 
+                  rounded-full ring-2 ring-zinc-900"
+                />
+              )}
             </div>
 
             {/* User info - only visible on larger screens */}
             <div className="hidden lg:block text-left min-w-0">
               <div className="font-medium truncate">{user.fullName}</div>
               <div className="text-sm text-zinc-400">
-                {onlineUsers.includes(user._id) ? 'Online' : 'Offline'}
+                {onlineUsers.includes(user._id)
+                  ? 'Online'
+                  : user?.id === 'chatbot'
+                  ? 'Online'
+                  : 'Offline'}
               </div>
             </div>
           </button>
--- frontend/src/store/useChatStore.ts
+++ frontend/src/store/useChatStore.ts
@@ -14,9 +14,18 @@ interface Message {
   senderId: string,
   text: string | null,
   image: string | null,
+  type?: "sent" | "received",
   createdAt: string,
   updatedAt: string
 }
+// interface ChatBotMessage {
+//   _id: string
+//   senderId: string,
+//   text: string | null,
+//   image: string | null,
+//   createdAt: string,
+//   updatedAt: string
+// }
 
 interface ChatInterface {
   messages: Message[]
@@ -27,8 +36,10 @@ interface ChatInterface {
   isMessageSending: boolean,
   getUsers: () => void,
   getMessages: (id: string) => void,
+  getChatbotMessages: () => void,
   setSelectedUser: (user: User | null) => void,
   sendMessage: (data: { text: string | null, image: string | ArrayBuffer | null }) => void,
+  sendChatBotMessage: (data: { text: string | null, image: string | ArrayBuffer | null }) => void,
   subscribeToMessages: () => void,
   unsubscribeFromMessages: () => void,
 }
@@ -45,7 +56,16 @@ export const useChatStore = create<ChatInterface>((set, get) => ({
     set({ isUsersLoading: true })
     try {
       const res = await apiClient.get("message/users")
-      set({ users: res.data })
+      const chatBotUser: User = {
+        _id: "chatbot",
+        id: "chatbot",
+        email: "test@chatbot.com",
+        fullName: "Chat Bot",
+        profilePic: "https://img.freepik.com/free-vector/graident-ai-robot-vectorart_78370-4114.jpg?semt=ais_hybrid&w=740",
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      }
+      set({ users: [...res.data, chatBotUser] })
       // toast.success("Users retrieved successfully!")
     } catch (error) {
       if (isMyApiError(error) && error.response) {
@@ -70,12 +90,39 @@ export const useChatStore = create<ChatInterface>((set, get) => ({
     }
   },
 
+  getChatbotMessages: async () => {
+    set({ isMessagesLoading: true })
+    try {
+      const res = await apiClient.get(`chatbot/`)
+      set({ messages: res.data })
+      // toast.success("Messages retrieved successfully!")
+    } catch (error) {
+      console.log("Error in fetching messages: ", error)
+    } finally {
+      set({ isMessagesLoading: false })
+    }
+  },
+
   sendMessage: async (data) => {
     set({ isMessageSending: true })
     const { selectedUser, messages } = get()
     try {
       const res = await apiClient.post(`message/users/${selectedUser!._id}`, data)
       set({ messages: [...messages, res.data] })
+      // toast.success("Message sent successfully!")
+    } catch (error) {
+      console.log("Error in posting message: ", error)
+    } finally {
+      set({ isMessageSending: false })
+    }
+  },
+
+  sendChatBotMessage: async (data) => {
+    set({ isMessageSending: true })
+    const { messages } = get()
+    try {
+      const res = await apiClient.post(`chatbot/`, data)
+      set({ messages: [...messages, res.data[0], res.data[1]] })
       // toast.success("Message sent successfully!")
     } catch (error) {
       console.log("Error in posting message: ", error)
